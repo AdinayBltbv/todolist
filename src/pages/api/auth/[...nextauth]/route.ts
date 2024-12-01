6117c15e39f44f32028dd8c3832e7672b95a75c8
AdinayBN <adinaybaltabaeva2004@gmail.com>
2024-12-01 15:29:59 +0600

    auth_google

---
+++ src/pages/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,13 @@
+// import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+// export const { GET, POST } = handlers;
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+});
